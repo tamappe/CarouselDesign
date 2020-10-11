e779b7d1ad8181058ce1c9679363277a69a59b31
tamaoki <treasure6hunter9cole@gmail.com>
2020-10-11 22:17:34 +0900

    first commit

---
+++ CarouselDesign/CarouselDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarouselDesign
+//
+//  Created by 玉置 on 2020/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
